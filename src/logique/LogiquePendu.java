f48967cd30d91d3b23cff3a550060fcb3c7a240b
Feral Theo <feralt@3il.fr>
2024-03-13 16:02:35 +0100

    Modification du code pour que le jeux puisse redémarer après une victoire ou une defaite.

--- src/logique/LogiquePendu.java
+++ src/logique/LogiquePendu.java
@@ -1,15 +1,19 @@
 package logique;
 
+import java.io.IOException;
+
 public class LogiquePendu {
     private String motSecret;
     private StringBuilder motAffiche;
+	private String definition;
 
-    public LogiquePendu(String motSecret) {
+	public LogiquePendu(String motSecret) {
         this.motSecret = motSecret.toUpperCase();
         this.motAffiche = new StringBuilder();
         for (int i = 0; i < motSecret.length(); i++) {
             motAffiche.append("_ ");
         }
+        this.definition = ""; // Initialiser la définition à une chaîne vide
     }
 
     public String getMotAffiche() {
@@ -33,5 +37,22 @@ public class LogiquePendu {
 
     public boolean isGameWin() {
         return motAffiche.indexOf("_") == -1;
+    }
+    
+    public void reset() {
+        try {
+            GestionMots gestionMots = new GestionMots();
+            String[] motAleatoire = gestionMots.tirerMotAleatoire();
+            this.motSecret = motAleatoire[0];
+            this.definition = motAleatoire[1];
+            // Réinitialiser motAffiche si nécessaire
+            this.motAffiche = new StringBuilder();
+            for (int i = 0; i < motSecret.length(); i++) {
+                motAffiche.append("_ ");
+            }
+        } catch (IOException e) {
+            System.err.println("Erreur lors de la récupération des mots aléatoires : " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
